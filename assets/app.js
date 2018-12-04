37a8e9837a73b6534b82cc7982cfdec6947565d8
Chris <chriskwong03@gmail.com>
2018-12-04 14:24:07 -0800

    Added Comments and responsive styles

--- assets/app.js
+++ assets/app.js
@@ -2,15 +2,7 @@ window.onload = () => {
   const dataPath = './assets/data/dogs.json';
   let dogsData = {};
 
-  fetch(dataPath)
-  .then(response => response.json())
-  .then(json => {
-    dogsData = json;
-    tilesContainer.removeChild(spinner);
-    render();
-  })
-  .catch(()=> alert("Oops! Something went wrong. Please refresh the page."));
-
+  /*--DOM Elements --------------------------------- */
   const dimmer = document.querySelector('.js-dimmer');
   const modal = document.querySelector('.js-modal');
   const imgContainer = document.querySelector('.js-img-container');
@@ -18,6 +10,10 @@ window.onload = () => {
   const tilesContainer = document.querySelector('.js-tiles-container');
   const spinner = document.querySelector('.js-spinner');
 
+
+  /*--Click Handlers --------------------------------- */
+
+  //Handles opening the modal - injects the selected image and adds `active` class to make modal visible.
   const clickHandler = (src, titleSrc) => {
     const modalImg = buildImg(src, 'modal__img', titleSrc, 'modal');
     imgContainer.appendChild(modalImg);
@@ -25,6 +21,7 @@ window.onload = () => {
     modal.classList.add('active');
   };
 
+  //Closing the modal should remove 'active' class to change visibility and clear the image for the next time the modal is activated. 
   const closeHandler = () => {
     const modalImg = document.querySelector('.modal__img');
     dimmer.classList.remove('active');
@@ -32,9 +29,15 @@ window.onload = () => {
     imgContainer.removeChild(modalImg);
   }
 
-  closeBtn.addEventListener("click", closeHandler);
-  dimmer.addEventListener("click", closeHandler);
 
+  /*--Event Listeners --------------------------------------- */
+  closeBtn.addEventListener("click", closeHandler); //closes modal
+  dimmer.addEventListener("click", closeHandler); //Allow clicking of area outside of the image to also close modal.
+
+
+  /*--Imperative DOM Creation Functions --------------------- */
+
+  // Creates 'img' DOM element and adds all relevant attributes.
   const buildImg = (src, className, titleSrc, type, index='') => {
     const img = document.createElement('img');
     img.classList.add(className);
@@ -44,18 +47,19 @@ window.onload = () => {
     return img;
   };
 
+  // Creates 'figure' DOM element as a wrapper for the img. 
   const buildTile = (tileImg, onCLickHandler) => {
     const tile = document.createElement('figure');
     tile.classList.add('tile');
     tile.onclick = onCLickHandler;
     tile.appendChild(tileImg);
     return tile;
-  }
+  };
 
   const render = () => {
     const {dogs} = dogsData;
     const tiles = dogs.map((dog, index) => {
-      const thumbsSrc = `.${dog.thumb}`;
+      const thumbsSrc = `.${dog.thumb}`; // '.' added to allow image to render locally
       const fullSizeSrc = `.${dog.image}`;
       const titleSrc = dog.source;
       const className = 'tile__img';
@@ -67,6 +71,20 @@ window.onload = () => {
     for (let tile of tiles) {
       tilesContainer.appendChild(tile);
     }
-  }
+  };
 
+  /*--AJAX --------------------------------- */
+
+  const fetchData = (dataPath) => {
+    fetch(dataPath)
+    .then(response => response.json())
+    .then(json => {
+      dogsData = json;
+      tilesContainer.removeChild(spinner); //remove loading spinner
+      render();
+    })
+    .catch(()=> alert("Oops! Something went wrong. Please refresh the page."));
+  }
+  //Initialize rendering
+  fetchData(dataPath);
 }
