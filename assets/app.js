bc7417c132bca6a777a51897bd95cf92841476d9
Chris <chriskwong03@gmail.com>
2018-12-04 09:38:23 -0800

    Added Low Res Thumbs

--- assets/app.js
+++ assets/app.js
@@ -13,81 +13,97 @@ window.onload = () => {
   const dogsData = {
     "dogs": [
     {
+        "thumb": "/assets/images/thumb/1.jpg",
         "image": "/assets/images/raw/1.jpeg",
         "source": "https://unsplash.com/photos/ybHtKz5He9Y"
       },
 
       {
+        "thumb": "/assets/images/thumb/2.jpg",
         "image": "/assets/images/raw/2.jpeg",
         "source": "https://unsplash.com/photos/rY79kwEEyGo"
       },
 
       {
+        "thumb": "/assets/images/thumb/3.jpg",
         "image": "/assets/images/raw/3.jpeg",
         "source": "https://unsplash.com/photos/ngqyo2AYYnE"
       },
 
       {
+        "thumb": "/assets/images/thumb/4.jpg",
         "image": "/assets/images/raw/4.jpeg",
         "source": "https://unsplash.com/photos/9A64wd1QaAU"
       },
 
       {
+        "thumb": "/assets/images/thumb/5.jpg",
         "image": "/assets/images/raw/5.jpeg",
         "source": "https://unsplash.com/photos/915UJQaxtrk"
       },
 
       {
+        "thumb": "/assets/images/thumb/6.jpg",
         "image": "/assets/images/raw/6.jpeg",
         "source": "https://unsplash.com/photos/mx0DEnfYxic"
       },
 
       {
+        "thumb": "/assets/images/thumb/7.jpg",
         "image": "/assets/images/raw/7.jpeg",
         "source": "https://unsplash.com/photos/OsOQhAzcEKc"
       },
 
       {
+        "thumb": "/assets/images/thumb/8.jpg",
         "image": "/assets/images/raw/8.jpeg",
         "source": "https://unsplash.com/photos/6WneSv56YVI"
       },
 
       {
+        "thumb": "/assets/images/thumb/9.jpg",
         "image": "/assets/images/raw/9.jpeg",
         "source": "https://unsplash.com/photos/CdVG9f96kyg"
       },
 
       {
+        "thumb": "/assets/images/thumb/10.jpg",
         "image": "/assets/images/raw/10.jpeg",
         "source": "https://unsplash.com/photos/bVA2DI7c9e8"
       },
 
       {
+        "thumb": "/assets/images/thumb/11.jpg",
         "image": "/assets/images/raw/11.jpeg",
         "source": "https://unsplash.com/photos/GvhSUEN-Lm8"
       },
 
       {
+        "thumb": "/assets/images/thumb/12.jpg",
         "image": "/assets/images/raw/12.jpeg",
         "source": "https://unsplash.com/photos/t2XHKIdc4Kw"
       },
 
       {
+        "thumb": "/assets/images/thumb/13.jpg",
         "image": "/assets/images/raw/13.jpeg",
         "source": "https://unsplash.com/photos/saRU3vzmgkY"
       },
 
       {
+        "thumb": "/assets/images/thumb/14.jpg",
         "image": "/assets/images/raw/14.jpeg",
         "source": "https://unsplash.com/photos/1z-kI6gIEpg"
       },
 
       {
+        "thumb": "/assets/images/thumb/15.jpg",
         "image": "/assets/images/raw/15.jpeg",
         "source": "https://unsplash.com/photos/zHZ2nTnBnAQ"
       }
     ]
   }
+
   const {dogs} = dogsData;
 
   const dimmer = document.querySelector('.js-dimmer');
@@ -129,10 +145,11 @@ window.onload = () => {
   }
 
   const tiles = dogs.map(dog => {
-    const src = `.${dog.image}`;
+    const thumbsSrc = `.${dog.thumb}`;
+    const fullSizeSrc = `.${dog.image}`;
     const className = 'tile__img';
-    const tileImg = buildImg(src, className);
-    const tile = buildTile(tileImg, () => clickHandler(src));
+    const tileImg = buildImg(thumbsSrc, className);
+    const tile = buildTile(tileImg, () => clickHandler(fullSizeSrc));
     return tile;
   });
 
